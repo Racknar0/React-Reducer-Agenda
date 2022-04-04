95fa951e7b1260ce7a897dd62cba6c8065087b53
Racknar0 <racknarow1@gmail.com>
2022-04-03 23:41:32 -0500

    Dynamic Form

--- src/Components/Contactos.jsx
+++ src/Components/Contactos.jsx
@@ -1,31 +1,34 @@
-import React, { useEffect, useReducer } from 'react'
-import { ContactosReducer } from '../reducers/ContactosReducer'
-import FormulariAdd from './FormulariAdd'
-import TablaContactos from './TablaContactos'
-
+import React, { useEffect, useReducer, useState } from 'react';
+import { ContactosReducer } from '../reducers/ContactosReducer';
+import FormulariAdd from './FormulariAdd';
+import TablaContactos from './TablaContactos';
 
 const Contactos = () => {
-
     const init = () => {
-        const contactos = localStorage.getItem('contactos')
+        const contactos = localStorage.getItem('contactos');
         console.log(contactos);
-        return contactos ? JSON.parse(contactos) : []
-    }
+        return contactos ? JSON.parse(contactos) : [];
+    };
 
     /* const [state, dispatch] = useReducer(ContactosReducer, []) */
-    const [state, dispatch] = useReducer(ContactosReducer, [], init)
-    
+    const [state, dispatch] = useReducer(ContactosReducer, [], init);
+
     useEffect(() => {
-      localStorage.setItem('contactos', JSON.stringify(state))
-    }, [state])
-    
+        localStorage.setItem('contactos', JSON.stringify(state));
+    }, [state]);
 
-  return (
-    <div className='container mt-3'>
-            <FormulariAdd dispatch={dispatch} />
+    const [formview, setFormview] = useState(false);
+
+    return (
+        <div className="container mt-3">
+            <button onClick={() => setFormview(!formview)} className='btn btn-success'>
+                {formview ? '- Cerrar Formulario' : '+ Agregar Contacto'}
+            </button>
+            {formview && <FormulariAdd dispatch={dispatch} />}
+
             <TablaContactos contactos={state} dispatch={dispatch} />
-    </div>
-  )
-}
+        </div>
+    );
+};
 
-export default Contactos
+export default Contactos;
